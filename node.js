6afdc8fb80452025366708f09fbbaa5178947d6d
1mDolphin <bulletin03@gmail.com>
2025-03-07 20:10:54 +0200

    REST API

--- node.js
+++ node.js
@@ -1,17 +1,110 @@
+const { DataTypes, Sequelize } = require('sequelize');
+
 const express = require('express');
 const app = express();
 app.use(express.json())
+
+const sequelize = new Sequelize({
+    dialect: 'sqlite',
+    storage: './database.sqlite'
+})
+
+const roles = ['user', 'admin', 'superadmin']
+const UserModel = sequelize.define('User', {
+    id: {
+        type: DataTypes.UUID,
+        defaultValue: DataTypes.UUIDV4,
+        primaryKey: true
+    },
+    username: {
+        type: DataTypes.STRING,
+        allowNull: false,
+        unique: true
+    },
+    email: {
+        type: DataTypes.STRING,
+        allowNull: false,
+        unique: true
+    },
+    password: {
+        type: DataTypes.STRING,
+        allowNull: false
+    },
+    role: {
+        type: DataTypes.ENUM(roles),
+        defaultValue: 'user'
+    }
+})
+
+const initialize = async () => {
+    await sequelize.sync({ force: false})
+}
+
+initialize()
+
+app.post('/users', async (req, res) => {
+    try {
+        const users = await UserModel.create(req.body)
+        res.status(201).json(users)
+    } catch (error) {
+        res.status(400).json({ error: error.message })
+    }
+})
+
+app.get('/users', async (req, res) => {
+    try {
+        const users = await UserModel.findAll()
+        res.json(users)
+    } catch (error) {
+        res.status(400).json({ error: error.message })
+    }
+})
+
+app.get('/users', async (req, res) => {
+    try {
+        const user = await UserModel.findByPk(req.params.id)
+        if (user) {
+            res.json(user)
+        } else {
+            req.status(404).json({ error: 'User not found' })
+        }
+    } catch (error) {
+        res.status(500).json({ error: error.message })
+    }
+})
+
+app.put('/users/:id', async (req, res) => {
+    try {
+        const user = await UserModel.findByPk(req.params.id)
+        if (user) {
+            await user.update(req.body)
+            res.json(user)
+        } else {
+            res.status(404).json({ error: 'User not found' })
+        } 
+    } catch (error) {
+        res.status(400).json({ error: error.message })
+    }
+})
+
+app.delete('/users/:id', async (req, res) => {
+    try {
+        const user = await UserModel.findByPk(req.params.id)
+        if (user) {
+            await user.destroy()
+            res.json({ message: 'User deleted' })
+            res.status(204).end()
+        } else {
+            res.status(404).json({ error: 'User not found' })
+        }
+    } catch {
+        res.status(500).json({ error: error.message })
+    }
+})
+
 const PORT = process.env.PORT || 8080;
+
 
 app.listen(PORT, () => {
     console.log(`Server is running on port ${PORT}`)
 })
-
-app.get("/users", (req, res) => {
-    const user = {
-        "Name": "username",
-        "Email": "email"
-    }
-
-    res.send(user)
-}) 
---
+++ seed.js
@@ -0,0 +1,64 @@
+const { Sequelize, DataTypes } = require('sequelize');
+const sequelize = new Sequelize({
+    dialect: 'sqlite',
+    storage: './database.sqlite'
+});
+
+const roles = ['user', 'admin', 'superadmin'];
+
+const UserModel = sequelize.define('User', {
+    id: {
+        type: DataTypes.UUID,
+        defaultValue: DataTypes.UUIDV4,
+        primaryKey: true
+    },
+    username: {
+        type: DataTypes.STRING,
+        allowNull: false,
+        unique: true
+    },
+    email: {
+        type: DataTypes.STRING,
+        allowNull: false,
+        unique: true
+    },
+    password: {
+        type: DataTypes.STRING,
+        allowNull: false
+    },
+    role: {
+        type: DataTypes.ENUM(roles),
+        defaultValue: 'user'
+    }
+});
+
+const initialize = async () => {
+    await sequelize.sync({ force: false });
+
+    await UserModel.bulkCreate([
+        {
+            username: 'user1',
+            email: 'user1@example.com',
+            password: 'password1',
+            role: 'user'
+        },
+        {
+            username: 'admin1',
+            email: 'admin1@example.com',
+            password: 'password2',
+            role: 'admin'
+        },
+        {
+            username: 'superadmin1',
+            email: 'superadmin1@example.com',
+            password: 'password3',
+            role: 'superadmin'
+        }
+    ]);
+
+    console.log('Users have been added');
+};
+
+initialize().catch(error => {
+    console.error('Error initializing database:', error);
+});
